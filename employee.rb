0815a8d97d4adf8de9c23c956e1ac15bc2cff65f
Pedro Chang <pedrochang91@gmail.com>
2022-04-26 11:29:45 -0700

    Implements using an array

---
+++ employee.rb
@@ -0,0 +1,5 @@
+employee1 = ["Suzie", "Pepper", 80000, true]
+employee2 = ["April", "Rhodes", 70000, true]
+
+puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year. "
+puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."

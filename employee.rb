1fa66c5607ae3e2e74b667c207e8681cdb596c0e
Pedro Chang <pedrochang91@gmail.com>
2022-04-26 13:27:00 -0700

    implements using class

--- employee.rb
+++ employee.rb
@@ -16,3 +16,22 @@ employee2 = { first_name: "April", last_name: "Rhodes", salary: 70000, active: t
 puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
 puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
 #symbols are used because they are more efficient, and uses less memory
+
+#4. Class time
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+end
+
+employee1 = Employee.new("Suzie", "Pepper", 80000, true)
+employee2 = Employee.new("April", "Rhodes", 70000, true)
+employee1.print_info
+employee2.print_info

8af58a2c2c6678162b6261652ab78832984bd5a2
Pedro Chang <pedrochang91@gmail.com>
2022-04-26 12:09:44 -0700

    made 3 items in hashes with symbols

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+item1 = { item: "pants", size: "large", brand: "Bloidy", price: 60, instock: true }
+item2 = { item: "guitar", size: "small", brand: "Martin", price: 10000, instock: true }
+item3 = { item: "mug", size: "perfect", brand: "Hyper Pure Ceramics", price: 1423, instock: true }
+puts "The #{item1[:item]} is/are #{item1[:size]} and the brand is #{item1[:brand]}. The cost is #{item1[:price]}."
+puts "The #{item2[:item]} is/are #{item2[:size]} and the brand is #{item2[:brand]}. The cost is #{item2[:price]}."
+puts "The #{item3[:item]} is/are #{item3[:size]} and the brand is #{item3[:brand]}. The cost is #{item3[:price]}."

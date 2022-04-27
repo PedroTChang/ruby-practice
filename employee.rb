32e96794e75154007aa5889a1087f6766910831b
Pedro Chang <pedrochang91@gmail.com>
2022-04-27 09:45:15 -0700

    Implements attr_reader and attr_writer

--- employee.rb
+++ employee.rb
@@ -1,10 +1,10 @@
-#1. Array
+#1. Array [easy to access, change]
 employee1 = ["Suzie", "Pepper", 80000, true]
 employee2 = ["April", "Rhodes", 70000, true]
 puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year. "
 puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
 
-#2. Hash
+#2. Hash [unordered]
 employee1 = { "first_name" => "Suzie", "last_name" => "Pepper", "salary" => 80000, "active" => true }
 employee2 = { "first_name" => "April", "last_name" => "Rhodes", "salary" => 70000, "active" => true }
 puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} a year."
@@ -17,8 +17,10 @@ puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:sala
 puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
 #symbols are used because they are more efficient, and uses less memory
 
-#4. Class time; allows you to say what is accessable
+#4. Class time; allows you to say what is accessable; long upfront; easier to keep creating new instances; more custom; encapsulation: allows you to control what one can access (no getter can't see it)
 class Employee
+  attr_reader :first_name, :last_name, :active # getter method
+
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
@@ -34,17 +36,17 @@ class Employee
     @salary = @salary * 1.05
   end
 
-  def first_name
-    return @first_name
-  end
+  # def first_name same as attr_reader
+  #   return @first_name
+  # end
 
-  def last_name
-    return @last_name
-  end
+  # def last_name
+  #   return @last_name
+  # end
 
-  def active
-    return @active
-  end
+  # def active
+  #   return @active
+  # end
 
   def active=(input_active)
     @active = input_active
@@ -58,4 +60,4 @@ employee2.print_info
 employee2.give_annual_raise
 employee2.print_info
 employee2.active = false
-p employee2
+puts employee1.first_name
--- store_item.rb
+++ store_item.rb
@@ -6,6 +6,9 @@ puts "The #{item2[:item]} is/are #{item2[:size]} and the brand is #{item2[:brand
 puts "The #{item3[:item]} is/are #{item3[:size]} and the brand is #{item3[:brand]}. The cost is #{item3[:price]}."
 
 class Item
+  attr_reader :item, :size, :brand, :price, :instock
+  attr_writer :instock
+
   def initialize(input_item, input_size, input_brand, input_price, input_instock)
     @item = input_item
     @size = input_size
@@ -15,28 +18,28 @@ class Item
   end
 
   def print_info
-    puts "The #{item} is/are #{size} and the brand is #{brand}. The cost is #{price}. It is currently #{@instock}"
+    puts "The #{item} is/are #{size} and the brand is #{brand}. The cost is #{price}."
   end
 
-  def item
-    return @item
-  end
+  # def item
+  #   return @item
+  # end
 
-  def size
-    return @size
-  end
+  # def size
+  #   return @size
+  # end
 
-  def brand
-    return @brand
-  end
+  # def brand
+  #   return @brand
+  # end
 
-  def price
-    return @price
-  end
+  # def price
+  #   return @price
+  # end
 
-  def instock
-    return @instock
-  end
+  # def instock
+  #   return @instock
+  # end
 
   def instock=(input_instock)
     @instock = input_instock
@@ -48,5 +51,7 @@ item2 = Item.new("guitar", "small", "Martin", 10000, true)
 item3 = Item.new("mug", "perfect", "Hyper Pure Ceramics", 1423, true)
 item1.print_info
 item2.print_info
-item3.instock = false
 item3.print_info
+item3.instock = false
+p item1.size
+p item3.instock

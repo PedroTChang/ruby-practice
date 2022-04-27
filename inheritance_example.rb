7ec75576139ece02a3be844cd99a59b999ea1fe2
Pedro Chang <pedrochang91@gmail.com>
2022-04-27 10:41:56 -0700

    Implements inheritance

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
   def initialize
     @speed = 0
     @direction = "north"
@@ -15,31 +15,28 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+class Car < Vehicle
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = "north"
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Vehicle
   def ring_bell
     puts "Ring ring!"
   end
 end
+
+bike = Bike.new
+bike.ring_bell
+p bike
+bike.accelerate
+p bike
+
+car = Car.new
+car.honk_horn
+p car
+car.accelerate
+p car

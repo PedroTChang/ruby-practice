d512622aef7bfd48bc423bb4b6fe3279137936f9
Pedro Chang <pedrochang91@gmail.com>
2022-04-27 20:50:21 -0700

    Implements loop a loop that prints hello 5 times

--- loops2_practice_problems.rb
+++ loops2_practice_problems.rb
@@ -6,6 +6,11 @@ while i < 11
 end
 
 # 2. Write a while loop that prints the word "hello" 5 times.
+i = 0
+while i < 5
+  p "hello"
+  i += 1
+end
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 

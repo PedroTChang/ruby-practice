6cd3d59a37a3952597c304524d554c377e3a47f7
Pedro Chang <pedrochang91@gmail.com>
2022-04-27 21:00:59 -0700

    Creates endless loop unless user inputs stop

--- loops2_practice_problems.rb
+++ loops2_practice_problems.rb
@@ -13,6 +13,13 @@ while i < 5
 end
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+while true
+  puts "Please, input stop or I will run forever!"
+  word = gets.chomp
+  if word == "stop"
+    break
+  end
+end
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 

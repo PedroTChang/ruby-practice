214d99eab8ec0cfa12d47539b5a86684f14b2633
Pedro Chang <pedrochang91@gmail.com>
2022-04-28 12:12:28 -0700

    creates numbers for foods in exercises_ruby_review.rb

--- exercises_ruby_review.rb
+++ exercises_ruby_review.rb
@@ -8,8 +8,16 @@ while index < 5
   index = index + 1
 end
 #1.2
-index_2 = 0
-while index_2 < foods.length
-  puts "I love " + foods[index_2]
-  index_2 += 1
+# index_2 = 0
+# while index_2 < foods.length
+#   puts "I love " + foods[index_2]
+#   index_2 += 1
+# end
+#1.3
+index_3 = 0
+number = 1
+while index_3 < foods.length
+  puts number.to_s + ". " + foods[index_3]
+  number += 1
+  index_3 += 1
 end

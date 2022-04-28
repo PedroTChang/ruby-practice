f778b1d7f7c66cf901c5ace491da9236fb7b8993
Pedro Chang <pedrochang91@gmail.com>
2022-04-28 11:58:05 -0700

    Adds loop stating I love for each food

--- exercises_ruby_review.rb
+++ exercises_ruby_review.rb
@@ -1,10 +1,15 @@
 puts "What is your favorite food?"
 # food = gets.chomp
 foods = []
-
+#1.1
 index = 0
 while index < 5
   foods << gets.chomp
   index = index + 1
 end
-p foods
+#1.2
+index_2 = 0
+while index_2 < foods.length
+  puts "I love " + foods[index_2]
+  index_2 += 1
+end
